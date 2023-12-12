37a35b820582fbcdc536bf74e9adb4b641819ae4
ksd1 <1614545312@qq.com>
2023-12-12 15:31:43 +0800

    添加指定参数选项

--- run.py
+++ run.py
@@ -4,10 +4,28 @@ from scheduler.sdts import SDTS
 from scheduler.heft import HEFT
 from scheduler.scheduler import Scheduler
 from util import *
+import argparse
+from types import SimpleNamespace
+
+def parse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d","--data", type=str, default="./data/data_2.txt")
+    parser.add_argument("-s","--scheduler", type=str, choices=['SDTS','HEFT'], default="SDTS")
+    args = parser.parse_args()
+    return {
+        "data": args.data,
+        "scheduler": args.scheduler
+    }
 
 if __name__ == '__main__':
-    data = Data("./data/data_2.txt")
-    draw_dag(data.G)
-    scheduler :Scheduler = SDTS(data)
-    # scheduler :Scheduler = HEFT(data)
-    scheduler.schedule()
+    config = SimpleNamespace(**parse())
+    print("data file : ", config.data)
+    print("scheduler: ", config.scheduler)
+    try:
+        data = Data(config.data)
+        draw_dag(data.G)
+        scheduler :Scheduler = eval(config.scheduler)(data)
+        scheduler.schedule()
+    except Exception as e:
+        print(e)
+        exit(1)
