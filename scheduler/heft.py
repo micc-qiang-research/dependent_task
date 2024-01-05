37ea891ede128739d4e8de5cbb5d9edbd5c1efe5
ksd1 <1614545312@qq.com>
2024-01-05 12:21:54 +0800

    heft适配接口

--- scheduler/heft.py
+++ scheduler/heft.py
@@ -3,6 +3,7 @@ from heft import heft, gantt
 import numpy as np
 import networkx as nx
 from util import *
+from .executor import Executor
 
 class HEFT(Scheduler):
     def __init__(self, data,config):
@@ -11,8 +12,8 @@ class HEFT(Scheduler):
         # 每个核作为一个proc，需要改写server_comm矩阵，假设每个edge server有两个核，cloud有|func|个核，即不限数量
         # TODO. 自定义核的数量
         self.func_number = self.N
-        self.cores = np.array(self.data.cores, dtype=int)
-        self.proc_number = np.sum(self.cores) + self.func_number
+        self.cores = np.array([s.core for s in self.servers], dtype=int)
+        self.proc_number = np.sum(self.cores)
         # self.proc_number = (self.server_comm.shape[0] - 1) * 2 + self.func_number
 
         self.total_process = self.get_total_process()
@@ -33,19 +34,17 @@ class HEFT(Scheduler):
         return self.func_process + prepare
 
 
-    def extend(self, comm, n):
+    def extend(self, arr):
         res = []
-        for idx, com in enumerate(comm[:-1]):
+        for idx, ele in enumerate(arr):
             for core in range(self.cores[idx]):
-                res.append(com)
-        for i in range(n):
-            res.append(comm[-1])
+                res.append(ele)
         return res
 
     def extend_process(self):
         total_process = []
         for process in self.total_process:
-            row = self.extend(process, self.func_number)
+            row = self.extend(process)
             total_process.append(row)
         total_process = np.array(total_process)
             
@@ -54,13 +53,10 @@ class HEFT(Scheduler):
 
     def extend_comm(self):
         server_comm = []
-        for idx,comm in enumerate(self.server_comm[:-1]):
-            row = self.extend(comm, self.func_number)
+        for idx,comm in enumerate(self.server_comm):
+            row = self.extend(comm)
             for core in range(self.cores[idx]):
                 server_comm.append(row)
-        row = self.extend(self.server_comm[-1], self.func_number)
-        for i in range(self.func_number):
-            server_comm.append(row)
         server_comm = np.array(server_comm)
         
         assert server_comm.shape[0] == self.proc_number, "server_comm error"
@@ -79,24 +75,29 @@ class HEFT(Scheduler):
     def is_deploy_in_cloud(self, core_id):
         return core_id >= self.proc_number - self.func_number
 
-    def trans_strategy(self, sched):
-        raw_strategy = [[] for i in range(self.proc_number)]
+    def output_scheduler_strategy(self):
+        sched = self.sched
+        replica = False
+        place = [[] for i in range(self.proc_number)]
+        download_sequence = None
+        
         for server in sched:
             for scheduler in sched[server]:
                 task = scheduler.task
                 proc = scheduler.proc
-                raw_strategy[proc].append(task)
-        super().trans_strategy(self.topology_gen_strategy(raw_strategy))
+                place[proc].append(task)
+
+        return replica, place, download_sequence, Executor.TOPOLOGY
 
     def schedule(self):
-        sched, task_sched, _ = heft.schedule_dag(self.G, 
+        self.sched, self.task_sched, _ = heft.schedule_dag(self.G, 
                             communication_matrix=self.server_comm, 
                             computation_matrix=self.total_process,communication_startup=np.zeros(self.server_comm.shape[0]))
-        # print(sched, task_sched)
-        self.trans_strategy(sched)
+        print(self.sched, self.task_sched)
+        return self.output_scheduler_strategy()
 
         # gantt.showGanttChart(sched)
         # self.trans_to_strategy(task_sched)
         # for s in self.strategy:
         #     s.debug()
-        self.show_result("heft")
+        # self.show_result("heft")
