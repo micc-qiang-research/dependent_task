2c69b4d899e93eb4e4d92c6fae1bab4a8d0426ec
ksd1 <1614545312@qq.com>
2024-04-08 21:21:05 +0800

    propse

--- scheduler/propose.py
+++ scheduler/propose.py
@@ -3,9 +3,10 @@ from typing import NamedTuple
 from docplex.mp.model import Model
 import networkx as nx
 from .scheduler import Scheduler
+from .sdtsPlus import SDTSPlus
 from .executor import Executor
 
-class Propose(Scheduler):
+class Propose(SDTSPlus):
     def is_func_has_layer(self, func, layer):
         if layer in self.funcs[func].layer:
             return 1
@@ -258,217 +259,131 @@ class Propose(Scheduler):
 
         return mdl, solution
 
-    def get_server_core(self, processor):
-        server_id = processor // self.max_core_number
-        core_id = processor % self.max_core_number
-        assert core_id < self.data.server_info[server_id][0]
-        return server_id, core_id
 
+    def get_func_layers(self, func_ids):
+        s = set()
+        for i in func_ids:
+            s = s.union(set(self.funcs[i].layer))
+        return list(s)
 
-    def random_get_solution(self, h, P, X):
-        X_ = np.zeros(np.array(X).shape)        
-        h_ = np.zeros(np.array(h).shape)
-        P_ = np.zeros(np.array(P).shape)
-        g_ = np.zeros((self.K, self.L))
-        g = np.zeros((self.K, self.L))
-
-        for i in self.range_func:
-            j = self.random_01_vector(X[i]) # choose server
-            k = self.random_01_vector(np.array(h[i][j])/X[i][j]) # choose core
-            X_[i][j] = 1
-            h_[i][j][k] = 1
-
-        for j in self.range_server:
-            for l in self.range_layer:
-                g[j][l] = min(sum([X_[i][j] for i in self.range_func if self.is_func_has_layer(i, l)]), 1)
-                # g_[j][l] = self.random_01(g[j][l])
-
-        for j in self.range_server:
-            for l1 in self.range_layer:
-                for l2 in range(l1, self.L):
-                    if g[j][l1] == 1 and g[j][l2] == 1:
-                        if l1 != l2:
-                            if P[j][l1][l2] + P[j][l2][l1] == 0:
-                                P_[j][l1][l2] = self.random_01(0.5)
-                            else:
-                                P_[j][l1][l2] = self.random_01(P[j][l1][l2]/(P[j][l1][l2] + P[j][l2][l1]))
-                            P_[j][l2][l1] = 1 - P_[j][l1][l2]
-                        else:
-                            P_[j][l1][l2] = 1
-        return h_, P_, X_
-
-    def random_01(self, prob):
-        if prob >= 1: prob = 1
-        return np.random.choice(2, p=[1-prob, prob])
+    def sequence_strategy(self, place):
+        pid = 0
+        for i in range(self.K):
+            funcs_idx = []
+            h = {}
+            for _ in range(self.servers[i].core):
+                funcs_idx.extend(place[pid])
+                for f in place[pid]:
+                    h[f] = pid
+                pid+=1 # 切换到下一个Processor
+            print(h, funcs_idx, self.get_func_layers(funcs_idx))
+            layers_idx = self.get_func_layers(funcs_idx)
+            self.solve_sequence_problem(h, i, funcs_idx, layers_idx)
+        exit(0)
     
-    def random_01_matrix(self, prob_matrix):
-        _, column = np.array(prob_matrix).shape
-        probs = np.array(prob_matrix).reshape(-1)
-        i = np.random.choice(range(len(probs)), p=probs)
-        return i // column, i % column
-    
-    def random_01_vector(self, prob_vector):
-        i = np.random.choice(range(len(prob_vector)), p=prob_vector)
-        return i
-                
-    def is_feasible(self, h, P, X, cnt=0, no_seq=True):
-        def print_(n):
-            print(n, "-----", cnt)
+    '''
+    @Params
+        h: h[i] 表示函数i的部署的核
+        n: 在哪台机器部署
+        funcs_idx: 部署到机器n的所有函数下标，如[0, 3, 4, 7]
+        layers_idx: 需要下载的layer, 如[1 ,3, 5, 7]，表示需要下载1,3,5,7
+    @Return
+        download sequence, 如
+    '''
+    def solve_sequence_problem(self, h, n, funcs_idx, layers_idx):
+        mdl = Model(name="propose_solver")
+        range_func = range(len(funcs_idx))
+        range_layer = range(len(layers_idx))
 
-        # x, h 放置决策
-        for i in self.range_func:
-            if sum([X[i][n] for n in self.range_server]) != 1:
-                print_(1)
-                return False
-            for n in self.range_server:
-                if sum([h[i][n][c] for c in self.range_core]) != X[i][n]:
-                    print_(2)
-                    return False
-                
-        if no_seq:
-            return True
+        ### 初始记映射关系
+        funcs_map = {} # func_id -> idx
+        for idx, fid in enumerate(funcs_idx):
+            funcs_map[fid] = idx
+        layers_map = {} # layer_id -> idx
+        for idx, lid in enumerate(layers_idx):
+            layers_map[lid] = idx
 
-        g = np.zeros((self.K, self.L))
-        for n in self.range_server:
-            for l in self.range_layer:
-                g[n][l] = min(sum([X[i][n] for i in self.range_func if self.is_func_has_layer(i, l)]),1)
+        def get_func_id(idx): return funcs_idx[idx]
+        def get_layer_id(idx): return layers_idx[idx]
 
-        # 镜像下载序列决策 1：存在关系
-        for n in self.range_server:          
-            for l1 in self.range_layer:
-                if g[n][l1] != P[n][l1][l1]:
-                    print_(3)
-                    return False
+        # p_l1_l2 机器n中镜像块l1和l2的下载序列关系
+        mdl.P = mdl.continuous_var_matrix(range_layer, range_layer, name=lambda ll: "p_%d_%d" % ll, ub=1)
 
-        # 镜像下载序列决策 2：序列关系
-        for n in self.range_server:
-            for l1 in self.range_layer:
-                for l2 in self.range_layer:
-                    if l1 != l2:
-                        if g[n][l1] == 0 or g[n][l2] ==0:
-                            if P[n][l1][l2] != 0 or P[n][l2][l1] != 0:
-                                print_(4)
-                                return False
-                        else:
-                            if P[n][l1][l2] + P[n][l2][l1] != 1:
-                                print_(5)
-                                return False
-                            
-        # 镜像下载序列决策 3： 不存在循环的序列拉取决策 A->B->C->A
-        for n in self.range_server:          
-            for l1 in self.range_layer:
-                for l2 in self.range_layer:
-                    for l3 in self.range_layer:
-                        if l1 != l2 != l3:
-                            if P[n][l1][l2] + P[n][l2][l3] + P[n][l3][l1] > 2:
-                                print_(6)
-                                return False
+        mdl.T_start = mdl.continuous_var_list(range_func, name=lambda l: "t_start_%d" % l) # 开始时间
+        mdl.T_end = mdl.continuous_var_list(range_func, name=lambda l: "t_end_%d" % l) # 结束时间
+        mdl.T_image = mdl.continuous_var_list(range_func, name=lambda l: "t_image_%d" % l) # 镜像准备好时间
+        mdl.TET = mdl.continuous_var()
 
-        return True
-    
-    def naive_strategy(self, h, P, X):
-        layers = [set() for _ in self.range_server] # 每个服务器需要下载的镜像集合
-        task_server_mapper = {}
-        download_sequence = []
-        for i,task in enumerate(h):
-            probs = np.array(task).reshape(-1)
-            processor = np.random.choice(len(probs), 1, p=probs)[0]
-            server_id, core_id = self.get_server_core(processor)
-            task_server_mapper[i] = (server_id, core_id)
-
-            for layer in range(self.data.L):
-                if layer in self.data.funcs[i].layer:
-                    layers[server_id].add(layer)
-
-        for i, server_p in enumerate(P):
-            layers_priority = np.array(server_p).sum(axis=1)
-            download_sequence.append(np.argsort(-layers_priority))
-
-        self.logger.debug(task_server_mapper)
-        self.logger.debug(download_sequence)
-        self.task_server_mapper = task_server_mapper
-        self.download_sequence = download_sequence
-
-
-    def random_rounding(self, h, P, X):
-        _h, _P, _X = self.random_get_solution(h,P,X)
-        cnt = 1
-        while not self.is_feasible(_h, _P, _X, cnt):
-            _h, _P, _X = self.random_get_solution(h,P,X)
-            cnt += 1
+        P,T_start,T_end,T_image,TET = mdl.P, mdl.T_start, mdl.T_end, mdl.T_image, mdl.TET 
         
-        h,P,X = _h, _P, _X
+        # (1) T_end
+        mdl.add_constraints((T_end[i] == T_start[i] + self.func_process[get_func_id(i)][n]) for i in range_func)
 
-        # print(X)
-        # for n in self.range_server:
-        #     print(f"P{n}", P[n])
+        # (2) 任务之间的序列关系
+        for i in range_func:
+            for func_j in self.G.predecessors(get_func_id(i)):
+                if(func_j in funcs_map):
+                    mdl.add_constraint(T_start[i] >= T_end[funcs_map[func_j]])
+
+        # (3) 
+        mdl.add_constraints(T_start[i] >= T_image[i] for i in range_func)
+
+        # (4)
+        mdl.add_constraints(TET >= T_end[i] for i in range_func)
+
+        # (5) T_image[i] -> 函数I的image准备好时间
+        for i in range_func:
+            # 获取当前func所需的所有layer
+            for layer_l in self.funcs[get_func_id(i)].layer:
+                mdl.add_constraint(\
+                T_image[i] >= self.servers[n].download_latency \
+                    * mdl.sum(P[l_,layers_map[layer_l]] \
+                            * self.layers[get_layer_id(l_)].size \
+                        for l_ in range_layer ))
         
-        task_server_mapper = {}
-        download_sequence = []
-        for i in self.range_func:
-            for n in self.range_server:
-                for c in self.range_core:
-                    if h[i][n][c] == 1:
-                        task_server_mapper[i] = (n,c)
+        # (6)
+        for i in range_func:
+            for j in range_func:
+                if(i!=j and h[get_func_id(i)] == h[get_func_id(j)]):
+                    mdl.add_constraint(mdl.max(T_start[i]-T_end[j], T_start[j] - T_end[i]) >= 0)
+
+        for l1 in range_layer:
+            for l2 in range_layer:
+                if(l1 == l2):
+                    # (9)
+                    mdl.add_constraint(P[l1,l1] == 1)
+                else:
+                    # (7)
+                    mdl.add_constraint(P[l1,l2] + P[l2, l1] == 1)
         
-        for n in self.range_server:
-            layers = []
-            for l in self.range_layer:
-                if P[n][l][l] == 1:
-                    layers.append(l)
+        for l1 in range_layer:
+            for l2 in range_layer:
+                for l3 in range_layer:
+                    if(l1!=l2!=l3):
+                        # (8)
+                        mdl.add_constraint(P[l1,l2] + P[l2, l3] + P[l3, l1] <= 2)
 
-            layers = sorted(layers, key=lambda l: sum(P[n][l]), reverse=True)
-            download_sequence.append(layers)
+        mdl.minimize(TET)
+        mdl.print_information()
+
+        solution = mdl.solve()
+
+        print(solution)
+
+        # P = [[mdl.P[l1,l2].solution_value for l2 in range_layer] for l1 in range_layer]
+        # self.logger.debug(f"P[{n}] = ")
+        # self.logger.debug(np.array(P))
+        exit(0)
 
 
-        self.logger.debug(task_server_mapper)
-        self.logger.debug(download_sequence)
-        self.task_server_mapper = task_server_mapper
-        self.download_sequence = download_sequence
-        
-        # self.get_makespan(task_server_mapper, download_sequence)
-            
-
-    def parse(self, mdl, solution):
-        if solution:
-            # self.logger.debug(solution)
-            h = []
-            for i in self.range_func:
-                h_ = [[mdl.h[i,n,c].solution_value for c in self.range_core] for n in self.range_server]
-                self.logger.debug(f"h[{i}] = ")
-                self.logger.debug(np.array(h_))
-                h.append(h_)
-            
-            X = [[mdl.X[i,n].solution_value for n in self.range_server] for i in self.range_func]
-
-            P = []
-            for n in self.range_server:
-                P_ = [[mdl.P[n,l1,l2].solution_value for l2 in self.range_layer] for l1 in self.range_layer]
-                self.logger.debug(f"P[{n}] = ")
-                self.logger.debug(np.array(P_))
-                P.append(P_)
-
-            exit(0)
-            self.random_rounding(h,P,X)
-        else:
-            self.logger.debug("求解失败")
-            exit(1)
-
-    def output_scheduler_strategy(self):
-        replica = False
-        place = [[] for i in range(self.cluster.get_total_core_number())]
-        # download_sequence = self.download_sequence
-        download_sequence = None # TODO. 先不采用镜像下载序列
-        for task_id in self.task_server_mapper:
-            server_id, core_id = self.task_server_mapper[task_id]
-            pid = self.cluster.get_total_core_id(server_id, core_id)
-            place[pid].append(task_id)
-        return replica, place, download_sequence, Executor.TOPOLOGY
 
     def __init__(self, data, config):
         super().__init__(data, config)
 
     def schedule(self):
-        mdl, solution = self.solve()
-        self.parse(mdl, solution)
-        return self.output_scheduler_strategy()
+        place = super().schedule()[1]
+        self.sequence_strategy(place)
+        exit(0)
+        # mdl, solution = self.solve()
+        # self.parse(mdl, solution)
+        # return self.output_scheduler_strategy()
